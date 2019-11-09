1fc8f2c6561fc4d3e416363f113e5c8ebfa99770
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-11-09 17:59:01 -0300

    Initial commit

---
+++ src/main/java/dev/soterocra/lab/gsmessagingstompwebsocket/GsMessagingStompWebsocketApplication.java
@@ -0,0 +1,13 @@
+package dev.soterocra.lab.gsmessagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsMessagingStompWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsMessagingStompWebsocketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/soterocra/lab/gsmessagingstompwebsocket/GsMessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soterocra.lab.gsmessagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsMessagingStompWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
